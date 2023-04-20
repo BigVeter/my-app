779b3972ec590e4c694a341150461ac18584368d
BigVeter <ivagin@yandex.ru>
2023-04-21 00:51:56 +0800

    Аттестация

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className='row row-col-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top'>
             <div className='col mb-3'>
                   <a className=''>
-                      <img src='img/logo1.png' className='bi me-2' width={180} height={180}>
+                      <img src='img/logo1.png' className='bi me-2' width={180} height={180}> 
                       </img>
                   </a>
                 <p>
